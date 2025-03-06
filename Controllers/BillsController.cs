b2925ade24b8d107d91fca1da076a77442e0734e
himanshu2285 <himanshu2k05m@gmail.com>
2025-03-06 23:48:37 +0530

    Add About and Bills functionality with migrations
    - Implemented About action in HomeController.
    - Created BillsController with Index action.
    - Updated AppDbContextModelSnapshot for ProductVersion.
    - Modified _Layout.cshtml to add navigation links.
    - Changed connection string in appsettings.json.
    - Added migration to allow null values for FullName.
    - Created Index.cshtml for Bills and About.cshtml for About Us page.

---
+++ Controllers/BillsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransportModule1.Controllers
+{
+    public class BillsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,10 @@ namespace TransportModule1.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [Authorize]
         public IActionResult Privacy()
